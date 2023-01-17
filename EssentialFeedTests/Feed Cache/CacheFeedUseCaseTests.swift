bebd30992ed307c2b37243dc890d8be256f64ca9
Ivo Dutra <ivogdutra@gmail.com>
2023-01-17 15:44:35 -0300

    Extract FeedCacheSpy into a shared scope to remove duplication

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -112,49 +112,6 @@ class CacheFeedUseCaseTests: XCTestCase {
      }
 }
 
-// MARK: - Spy
-private extension CacheFeedUseCaseTests {
-    class FeedStoreSpy: FeedStore {
-        typealias DeletionCompletion = (Error?) -> Void
-        typealias InsertionCompletion = (Error?) -> Void
-        
-        private(set) var receivedMessages: [ReceivedMessage] = []
-        private var deletionCompletions = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-        
-        enum ReceivedMessage: Equatable {
-            case deleteCachedFeed
-            case insert(feed: [LocalFeedImage], timestamp: Date)
-        }
-        
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            receivedMessages.append(.deleteCachedFeed)
-            deletionCompletions.append(completion)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insert(feed: feed, timestamp: timestamp))
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
-}
-
 // MARK: - Helpers
 
 private extension CacheFeedUseCaseTests {
