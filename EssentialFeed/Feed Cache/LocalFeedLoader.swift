9b8805526dea36501a09d4f5fed29b78a192681e
Ivo Dutra <ivogdutra@gmail.com>
2023-01-20 17:36:30 -0300

    Validate cache command does not delete cache when cache is already empty

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -40,11 +40,11 @@ public final class LocalFeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self else { return }
-
+            
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-           
+                
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
                 
@@ -59,33 +59,38 @@ public final class LocalFeedLoader {
     }
     
     public func validateCache() {
-         store.retrieve { _ in }
-         store.deleteCachedFeed { _ in }
-     }
+        store.retrieve { [unowned self] result in
+            switch result {
+            case .failure:
+                self.store.deleteCachedFeed { _ in }
+            default: break
+            }
+        }
+    }
     
     // MARK: - Helpers
     
     private func cache(_ images: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
         store.insert(images.toLocal(), timestamp: currentDate()) { [weak self] error in
-             guard self != nil else { return }
-
-             completion(error)
-         }
-     }
+            guard self != nil else { return }
+            
+            completion(error)
+        }
+    }
     
     private func validate(_ timestamp: Date) -> Bool {
-         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-             return false
-         }
-         return currentDate() < maxCacheAge
-     }
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
+    }
 }
 
 private extension Array where Element == FeedImage {
-     func toLocal() -> [LocalFeedImage] {
-         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-     }
- }
+    func toLocal() -> [LocalFeedImage] {
+        return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
 
 private extension Array where Element == LocalFeedImage {
     func toModels() -> [FeedImage] {
