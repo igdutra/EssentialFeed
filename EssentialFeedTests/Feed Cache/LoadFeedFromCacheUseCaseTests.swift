3c6ad253ab406543041db11c52d80cee8fc75b24
Ivo Dutra <ivogdutra@gmail.com>
2023-01-20 17:47:06 -0300

    Extract duplicate test helpers into a shared scope

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -193,26 +193,4 @@ private extension LoadFeedFromCacheUseCaseTests {
         
         wait(for: [exp], timeout: 1.0)
     }
-    
-    func uniqueImage() -> FeedImage {
-        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-    }
-    
-    func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-        let models = [uniqueImage(), uniqueImage()]
-        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-        return (models, local)
-    }
-}
-
-// MARK: - Date Extension
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        self + seconds
-    }
 }
--- EssentialFeedTests/Helper/SharedTestHelpers.swift
+++ EssentialFeedTests/Helper/SharedTestHelpers.swift
@@ -16,14 +16,15 @@ public extension XCTestCase {
     
     // MARK: - Stubs
     
-    func anyURL(_ host: String = "a-url.com") -> URL {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = host
-        return components.url!
-    }
-    
     func anyNSError() -> NSError {
         NSError(domain: "any error", code: 1)
     }
+}
+
+// URL had to be moved out in order to be used in uniqueItems()
+func anyURL(_ host: String = "a-url.com") -> URL {
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = host
+    return components.url!
 }
