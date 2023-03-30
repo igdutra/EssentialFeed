5102a943bdbe90caf6e8d9b50d8bc0c35f29f5e8
Ivo Dutra <ivogdutra@gmail.com>
2023-03-30 17:56:30 -0300

    Add swiftUI prototype

---
+++ SwiftUIPrototype/SwiftUIPrototype/SwiftUIPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPrototypeApp.swift
+//  SwiftUIPrototype
+//
+//  Created by Ivo on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
