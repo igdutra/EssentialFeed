5833d5cc7b24810ba4ac5506cc1cea6eb170e660
Ivo Dutra <ivogdutra@gmail.com>
2023-03-31 10:03:05 -0300

    Add prototype feed data for demoing purpose

---
+++ Prototypes/UIKitPrototype/UIKitPrototype/FeedImageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ivo on 30/03/23.
+//
+
+import Foundation
+
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
