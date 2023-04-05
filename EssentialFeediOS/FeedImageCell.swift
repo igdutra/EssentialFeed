72b14ec39ae286af8eb4cdb54198dc7b272438d2
Ivo Dutra <ivogdutra@gmail.com>
2023-04-05 12:52:24 -0300

    Render loaded feed on successful load completion -> big commit TDD flow explained

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Ivo on 05/04/23.
+//
+
+import UIKit.UITableViewCell
+
+ public final class FeedImageCell: UITableViewCell {
+     public let locationContainer = UIView()
+     public let locationLabel = UILabel()
+     public let descriptionLabel = UILabel()
+ }
