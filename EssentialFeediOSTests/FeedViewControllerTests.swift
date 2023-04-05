19af0159d1c8042ad4141d35f8a7d7f228a47f10
Ivo Dutra <ivogdutra@gmail.com>
2023-04-05 13:11:21 -0300

    IMPORTANT: Extract cancelImageDataLoad(from: URL) method from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that represents a task that can be cancelled. This way, we respect the Interface Segregation Principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -114,35 +114,39 @@ extension FeedViewControllerTests {
         // MARK: - FeedLoader
         
         private var feedRequests = [(FeedLoader.Result) -> Void]()
-
+        
         var loadFeedCallCount: Int {
             return feedRequests.count
         }
-
+        
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             feedRequests.append(completion)
         }
-
+        
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
             feedRequests[index](.success(feed))
         }
-
+        
         func completeFeedLoadingWithError(at index: Int = 0) {
             let error = NSError(domain: "an error", code: 0)
             feedRequests[index](.failure(error))
         }
-
+        
         // MARK: - FeedImageDataLoader
+        
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            let cancelCallback: () -> Void
+            func cancel() {
+                cancelCallback()
+            }
+        }
         
         private(set) var loadedImageURLs = [URL]()
         private(set) var cancelledImageURLs = [URL]()
-
-        func loadImageData(from url: URL) {
-            loadedImageURLs.append(url)
-        }
         
-        func cancelImageDataLoad(from url: URL) {
-            cancelledImageURLs.append(url)
+        func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
+            loadedImageURLs.append(url)
+            return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
         }
     }
 }
