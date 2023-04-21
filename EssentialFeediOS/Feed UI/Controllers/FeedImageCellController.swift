ce4b94946fb9c36276cb5bbbec5e97856c5f1aa1
Ivo Dutra <ivogdutra@gmail.com>
2023-04-21 15:06:11 -0300

    UI: Adding final controllers

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedImageCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Ivo on 21/04/23.
+//
+
+import UIKit
+
+protocol FeedImageCellControllerDelegate {
+    func didRequestImage()
+    func didCancelImageRequest()
+}
+
+final class FeedImageCellController: FeedImageViewOld {
+    private let delegate: FeedImageCellControllerDelegate
+    private var cell: FeedImageCell?
+    
+    init(delegate: FeedImageCellControllerDelegate) {
+        self.delegate = delegate
+    }
+    
+    func view(in tableView: UITableView) -> UITableViewCell {
+        cell = tableView.dequeueReusableCell()
+        delegate.didRequestImage()
+        return cell!
+    }
+    
+    func preload() {
+        delegate.didRequestImage()
+    }
+    
+    func cancelLoad() {
+        releaseCellForReuse()
+        delegate.didCancelImageRequest()
+    }
+    
+    func display(_ viewModel: FeedImageMVPViewModel<UIImage>) {
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
+        cell?.feedImageView.setImageAnimated(viewModel.image)
+        cell?.feedImageContainer.isShimmeringMVVM = viewModel.isLoading
+        cell?.feedImageRetryButton.isHidden = !viewModel.shouldRetry
+        cell?.onRetry = delegate.didRequestImage
+    }
+    
+    private func releaseCellForReuse() {
+        cell = nil
+    }
+}
