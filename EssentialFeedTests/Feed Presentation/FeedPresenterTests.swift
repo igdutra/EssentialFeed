73916480302e9d838124c1da72ed3d189a5d8587
Ivo Dutra <ivogdutra@gmail.com>
2023-04-21 12:36:50 -0300

    FeedPresenter displays no error on didStartLoadingFeed

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,27 @@
 
 import XCTest
 
+struct FeedErrorViewData {
+    let message: String?
+
+    static var noError: FeedErrorViewData {
+        return FeedErrorViewData(message: nil)
+    }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewData)
+}
+
 final class FeedPresenter {
-    init(view: Any) {
-        
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
     }
 }
 
@@ -21,21 +39,42 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
-    // MARK: - Helpers
-    
-    private class ViewSpy {
-        let messages = [Any]()
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
     }
     
+    // MARK: - Helpers
+    
+    private class ViewSpy: FeedErrorView {
+        /* NOTE Message Array
+         
+         So use it but to avoid confromance with Equatable, just use the elements inside the viewModel not
+         the complete method signture
+         
+         */
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ viewModel: FeedErrorViewData) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
+    }
 }
 
 // MARK: - Helpers
 private extension FeedPresenterTests {
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
-         let view = ViewSpy()
-         let sut = FeedPresenter(view: view)
-         trackForMemoryLeaks(view, file: file, line: line)
-         trackForMemoryLeaks(sut, file: file, line: line)
-         return (sut, view)
-     }
+        let view = ViewSpy()
+        let sut = FeedPresenter(errorView: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
 }
