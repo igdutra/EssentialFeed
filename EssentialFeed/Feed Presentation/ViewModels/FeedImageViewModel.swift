3a382501e5fd726c9d4793fcd941ef39fc6ac5f5
Ivo Dutra <ivogdutra@gmail.com>
2023-04-21 16:39:46 -0300

    Move FeedImageViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/ViewModels/FeedImageViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ivo on 21/04/23.
+//
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
