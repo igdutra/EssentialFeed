ba0422c14313871102be882dcbf01d8674db43cb
Ivo Dutra <ivogdutra@gmail.com>
2023-04-21 15:25:39 -0300

    Final UI: change the tests so that it reflects the final `FeedViewController`

--- EssentialFeediOSTests/Feed UI/Helpers/Test-specific DSLs/FeedViewController+DSL.swift
+++ EssentialFeediOSTests/Feed UI/Helpers/Test-specific DSLs/FeedViewController+DSL.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EssentialFeediOS
  
-extension FeedViewControllerStoryboard {
+extension FeedViewController {
     func simulateUserInitiatedFeedReload() {
         refreshControl?.simulatePullToRefresh()
     }
