af4332d9d79972a7cc922d57f9be98d90de9c81d
Ivo Dutra <ivogdutra@gmail.com>
2023-04-26 12:55:57 -0300

    Re-write TestPlan so that it is possible to execute it in random order, and Add it to the CI iOS Scheme. Remove SceneDelegate to see the failing test

--- EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
+++ EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
@@ -9,5 +9,13 @@ import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
     
-    
+    // Failing Test
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+
+        app.launch()
+
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+    }
 }
