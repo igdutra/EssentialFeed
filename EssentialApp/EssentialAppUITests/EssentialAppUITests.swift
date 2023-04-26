455e18d784cb4c730c160fde9febb4016622d606
Ivo Dutra <ivogdutra@gmail.com>
2023-04-26 14:13:13 -0300

    Displays empty feed when customer has no connectivity and no cache

--- EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
+++ EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
@@ -7,6 +7,14 @@
 
 import XCTest
 
+/* NOTE
+ 
+ Maybe do some dictionary holding references to the Accessibility labels (static enum containing it
+ OR that the default identifier is the class name + "accessibilityIdentifier" can create a free function that does that.
+ 
+ Also an enum holding static properties to the launchArguments is a good option.
+ 
+ */
 final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
@@ -21,7 +29,6 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         XCTAssertTrue(firstImage.exists)
     }
     
-    
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
         onlineApp.launch()
@@ -35,5 +42,14 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
+    }
+    
+    func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+        let app = XCUIApplication()
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
+        app.launch()
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 0)
     }
 }
