e8bc6b629e76167a5925e3d369a509ab35410fb4
Ivo Dutra <ivogdutra@gmail.com>
2023-05-02 17:46:20 -0300

    Moving to Combine: Duplicated SceneDelegate

---
+++ EssentialApp/EssentialApp/Old Desing Patterns substituted by Combine/SceneDelegateWithoutCombine.swift
@@ -0,0 +1,75 @@
+//
+//  SceneDelegateWithoutCombine.swift
+//  EssentialApp
+//
+//  Created by Ivo on 02/05/23.
+//
+
+import UIKit
+import CoreData
+import EssentialFeed
+
+class SceneDelegateWithoutCombine: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    /* NOTE Lazy instantiation
+     
+     If they are not set, we have a chance of instantiating them lazyly
+     
+     So lazy vars is our recommended way of creating expensive dependencies like CoreData instances that need to open DB connections and load data from disk.
+     This way, you only run expensive operations when really needed.
+     
+     */
+    private lazy var httpClient: HTTPClient = {
+        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }()
+    
+    private lazy var store: FeedStore & FeedImageDataStore = {
+        try! CoreDataFeedStore(storeURL:
+                                NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite"))
+    }()
+    
+    private lazy var localFeedLoader: LocalFeedLoader = {
+        LocalFeedLoader(store: store, currentDate: Date.init)
+    }()
+    
+    convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
+        self.init()
+        self.httpClient = httpClient
+        self.store = store
+    }
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+        
+        let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: store)
+        
+        let feedLoader = FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader,
+                                                                                           cache: localFeedLoader),
+                                                         fallback: localFeedLoader)
+        let imageLoader = FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader,
+                                                                   fallback: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader,
+                                                                                                               cache: localImageLoader))
+        window?.rootViewController = UINavigationController(rootViewController:
+                                                                FeedUIComposer.feedComposedWith(feedLoader: feedLoader,
+                                                                                                imageLoader: imageLoader))
+        window?.makeKeyAndVisible()
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        localFeedLoader.validateCache { _ in }
+    }
+}
