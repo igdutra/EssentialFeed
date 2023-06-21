37350b58c1940bef8edaa2044d5bf2cf41899653
Ivo Dutra <ivogdutra@gmail.com>
2023-06-21 19:03:28 -0300

    Replace "FEED_VIEW_CONNECTION_ERROR" key with "GENERIC_CONNECTION_ERROR"

--- EssentialFeed/EssentialFeediOS/Old implementations/Feed Presentation/FeedRefreshPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Old implementations/Feed Presentation/FeedRefreshPresenter.swift
@@ -43,7 +43,7 @@ final class FeedRefreshPresenter {
     }
     
     private var feedLoadError: String {
-        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+        return NSLocalizedString("GENERIC_CONNECTION_ERROR",
                                  tableName: "Feed",
                                  bundle: Bundle(for: FeedRefreshPresenter.self),
                                  comment: "Error message displayed when we can't load the image feed from the server")
