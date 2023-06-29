8469166a30b4698d2dd386f430615a7278a10c65
Ivo Dutra <ivogdutra@gmail.com>
2023-06-29 19:25:09 -0300

    Inline param

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -26,8 +26,8 @@ class FeedPresenterTests: XCTestCase {
 // MARK: - Helpers
 private extension FeedPresenterTests {
     func localized(_ key: String,
-                   table: String = "Feed",
                    file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
