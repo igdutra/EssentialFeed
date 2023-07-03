9495687cf9891c8eb662854824d2e6625b102c5a
Ivo Dutra <ivogdutra@gmail.com>
2023-07-03 18:35:42 -0300

    Fix UI module tests after adding new FeedImagePresenter

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/DeletedFiles/FeedImagePresenterTests-BeforeImageFeature.swift
@@ -0,0 +1,125 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Ivo on 21/04/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImagePresenterTestsBeforeImageFeature: XCTestCase {
+    
+    func test_map_createsViewModel() {
+        let image = uniqueImage()
+        let viewModel = FeedImagePresenterBeforeImageFeature<ViewSpy, AnyImage>.map(image)
+        
+        XCTAssertEqual(viewModel.description, image.description)
+        XCTAssertEqual(viewModel.location, image.location)
+    }
+    
+    func test_init_doesNotSendMessagesToView() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+        
+        sut.didStartLoadingImageData(for: image)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertNil(message?.image)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.shouldRetry, false)
+    }
+    
+    func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
+        let (sut, view) = makeSUT(imageTransformer: fail)
+        let image = uniqueImage()
+        
+        sut.didFinishLoadingImageData(with: Data(), for: image)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertNil(message?.image)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, true)
+    }
+    
+    func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
+        let image = uniqueImage()
+        let transformedData = AnyImage()
+        let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
+        
+        sut.didFinishLoadingImageData(with: Data(), for: image)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertEqual(message?.image, transformedData)
+    }
+    
+    func test_didFinishLoadingImageData_displaysRetryButton() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+        
+        sut.didFinishLoadingImageData(with: anyNSError(), for: image)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertNil(message?.image)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, true)
+    }
+}
+
+// MARK: - Spy
+private extension FeedImagePresenterTestsBeforeImageFeature {
+    class ViewSpy: FeedImageView {
+        /* NOTE no silver bullet
+         
+         Instead of adding extra enum for each method call, just an enum containing the viewModels
+         
+         that's because the ViewControllerSpy implemented 3 protocols at the same time, here there's only one protocol
+         
+         */
+        private(set) var messages = [FeedImageViewModelBeforeImageFeature<AnyImage>]()
+        
+        func display(_ model: FeedImageViewModelBeforeImageFeature<AnyImage>) {
+            messages.append(model)
+        }
+    }
+}
+
+// MARK: - Helpers
+private extension FeedImagePresenterTestsBeforeImageFeature {
+    struct AnyImage: Equatable { }
+    
+    private func makeSUT(imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil },
+                         file: StaticString = #file,
+                         line: UInt = #line)
+    -> (sut: FeedImagePresenterBeforeImageFeature<ViewSpy, AnyImage>, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenterBeforeImageFeature(view: view, imageTransformer: imageTransformer)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private var fail: (Data) -> AnyImage? {
+        return { _ in nil }
+    }
+}
