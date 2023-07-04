0129c36b522df9b270d1db1eaa36c49a9af1f3c0
Ivo Dutra <ivogdutra@gmail.com>
2023-07-04 16:55:51 -0300

    Extract shared ListViewController snapshot tests

---
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTest.swift
@@ -0,0 +1,64 @@
+//
+//  ListSnapshotTest.swift
+//  EssentialFeediOSTests
+//
+//  Created by Ivo on 04/07/23.
+//
+
+import XCTest
+import EssentialFeediOS
+
+final class ListSnapshotTest: XCTestCase {
+
+    /* NOTE
+     
+     1 - first record the view using
+     record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+     
+     2- Once recorded, assert it
+     assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+     
+     */
+    
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
+    }
+    
+    /* NOTE temporarily disbling this test
+     
+     Disabling this test since is the only snapshot failing, even when i'm using
+     Xcode14.2 + Iphone 14 - iOS 16.2
+     
+     THE SNAPSHOT WILL PASS ONLY when activated the tolerance! if !match(snapshotData, storedSnapshotData, tolerance: 0.00001)
+     */
+    func test_feedWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+}
