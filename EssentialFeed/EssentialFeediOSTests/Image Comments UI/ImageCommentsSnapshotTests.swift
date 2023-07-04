bc994d5addd952c7c07d08b05ab3de419a082419
Ivo Dutra <ivogdutra@gmail.com>
2023-07-04 17:13:41 -0300

    Created both FeedUI and ImageComments UI folders in test target

---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Ivo on 04/07/23.
+//
+
+import XCTest
+
+final class ImageCommentsSnapshotTests: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test.")
+    }
+}
