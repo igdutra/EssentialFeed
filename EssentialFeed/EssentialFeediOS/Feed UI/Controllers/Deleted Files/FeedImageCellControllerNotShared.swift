94ce997ee154b28d712bfc9d3cf43326ccd351bf
Ivo Dutra <ivogdutra@gmail.com>
2023-07-06 07:54:47 -0300

    Commit the copy `FeedImageCellControllerNotShared`

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/Deleted Files/FeedImageCellControllerNotShared.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/Deleted Files/FeedImageCellControllerNotShared.swift
@@ -5,4 +5,59 @@
 //  Created by Ivo on 04/07/23.
 //
 
-import Foundation
+import UIKit
+import EssentialFeed
+
+public protocol FeedImageCellControllerDelegateNotShared {
+    func didRequestImage()
+    func didCancelImageRequest()
+}
+
+public final class FeedImageCellControllerNotShared: ResourceView, ResourceLoadingView, ResourceErrorView {
+    public typealias ResourceViewModel = UIImage
+    
+    private let viewModel: FeedImageViewModel
+    private let delegate: FeedImageCellControllerDelegateNotShared
+    private var cell: FeedImageCell?
+    
+    public init(viewModel: FeedImageViewModel, delegate: FeedImageCellControllerDelegateNotShared) {
+        self.viewModel = viewModel
+        self.delegate = delegate
+    }
+    
+    func view(in tableView: UITableView) -> UITableViewCell {
+        cell = tableView.dequeueReusableCell()
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
+        cell?.feedImageView.image = nil
+        cell?.onRetry = delegate.didRequestImage
+        delegate.didRequestImage()
+        return cell!
+    }
+    
+    func preload() {
+        delegate.didRequestImage()
+    }
+    
+    func cancelLoad() {
+        releaseCellForReuse()
+        delegate.didCancelImageRequest()
+    }
+    
+    public func display(_ viewModel: UIImage) {
+        cell?.feedImageView.setImageAnimated(viewModel)
+    }
+    
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell?.feedImageContainer.isShimmeringMVVM = viewModel.isLoading
+    }
+    
+    public func display(_ viewModel: ResourceErrorViewModel) {
+        cell?.feedImageRetryButton.isHidden = viewModel.message == nil
+    }
+    
+    private func releaseCellForReuse() {
+        cell = nil
+    }
+}
