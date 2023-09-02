48f91bdb191a627e2db28551faf238491c4036c1
Ivo Dutra <ivogdutra@gmail.com>
2023-09-02 18:19:39 -0300

    Set FeedEndpoint limit to 10

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -13,7 +13,16 @@ public enum FeedEndpoint {
     public func url(baseURL: URL) -> URL {
         switch self {
         case .get:
-            return baseURL.appendingPathComponent("/v1/feed")
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            // Note:
+            // Decided to force-unwrap because if no valid base-url is provided, is developer error and there are unit tests to back it up
+            return components.url!
         }
     }
 }
