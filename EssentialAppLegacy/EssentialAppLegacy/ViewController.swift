7a31fff5460cac5b441053948806b73bf949abe3
Ivo Dutra <ivogdutra@gmail.com>
2023-09-14 17:46:58 -0300

    Add new App Project: "EssentialAppLegacy"

---
+++ EssentialAppLegacy/EssentialAppLegacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EssentialAppLegacy
+//
+//  Created by Ivo on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
