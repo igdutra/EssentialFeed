ecb5ae52f4f509a8f8d9b02e9d601389c984c99d
Ivo Dutra <ivogdutra@gmail.com>
2023-09-15 15:29:12 -0300

    Add Legacy UI test target to run hig-Level Acceptance Tests

---
+++ EssentialAppLegacy/EssentialAppLegacyUIAcceptanceTests/EssentialAppLegacyUIAcceptanceTests.swift
@@ -0,0 +1,22 @@
+//
+//  EssentialAppLegacyUIAcceptanceTests.swift
+//  EssentialAppLegacyUIAcceptanceTests
+//
+//  Created by Ivo on 15/09/23.
+//
+
+import XCTest
+
+final class EssentialAppLegacyUIAcceptanceTests: XCTestCase {
+
+
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
