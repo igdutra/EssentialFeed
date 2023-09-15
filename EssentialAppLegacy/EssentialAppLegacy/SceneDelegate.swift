21f01199d539d4ba0d231af5d21df5fdfbc9e128
Ivo Dutra <ivogdutra@gmail.com>
2023-09-14 23:34:44 -0300

    Added DebuggingSceneDelegate

--- EssentialAppLegacy/EssentialAppLegacy/SceneDelegate.swift
+++ EssentialAppLegacy/EssentialAppLegacy/SceneDelegate.swift
@@ -9,29 +9,32 @@ import UIKit
 import CoreData
 import EssentialFeed
 
+/* NOTE Scene Delegate for UI Testing
+ 
+ The below configuration was changed to that it allowed DebbugingSceneDelegate
+ 
+ 1- LocalStoreURL
+ 2- makeRemoteClient
+ 
+ */
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    private lazy var httpClient: HTTPClient = {
-        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-    }()
+
     
+    let localStoreURL = NSPersistentContainer
+        .defaultDirectoryURL()
+        .appendingPathComponent("legacy-feed-store.sqlite")
+    
+    // Note: removed private to DebbuggingSceneDelegate
     private lazy var store: FeedStore & FeedImageDataStore = {
         try! CoreDataFeedStore(
-            storeURL: NSPersistentContainer
-                .defaultDirectoryURL()
-                .appendingPathComponent("legacy-feed-store.sqlite"))
+            storeURL: localStoreURL)
     }()
     
     private lazy var localFeedLoader: LocalFeedLoader = {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }()
-    
-    convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
-        self.init()
-        self.httpClient = httpClient
-        self.store = store
-    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
@@ -43,8 +46,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func configureWindow() {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+        let remoteClient = makeRemoteClient()
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
         let localImageLoader = LocalFeedImageDataLoader(store: store)
         
         let feedLoader = FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader,
@@ -65,4 +69,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         localFeedLoader.validateCache { _ in }
     }
+    
+    // MARK: - Helpers
+    func makeRemoteClient() -> HTTPClient {
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }
+    
+    // makeRemoteClient vs
+//    private lazy var httpClient: HTTPClient = {
+//        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+//    }()
 }
