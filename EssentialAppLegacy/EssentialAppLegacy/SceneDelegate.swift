2ebdf58b40fb6575d1be31a67ee32dee478f8e8c
Ivo Dutra <ivogdutra@gmail.com>
2023-09-14 22:30:16 -0300

    Add Legacy Controllers, Views, Storyboard: make basic Legacy UI from feed appear

--- EssentialAppLegacy/EssentialAppLegacy/SceneDelegate.swift
+++ EssentialAppLegacy/EssentialAppLegacy/SceneDelegate.swift
@@ -9,6 +9,12 @@ import UIKit
 import CoreData
 import EssentialFeed
 
+struct Delegate: FeedViewControllerDelegate {
+    func didRequestFeedRefresh() {
+        print("print")
+    }
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
@@ -18,10 +24,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let firstVC = UIViewController()
-        firstVC.view.backgroundColor = .green
-        window.rootViewController = firstVC
+
+        let feed = makeFeedViewController(delegate: Delegate(), title: "Feed")
+        let root = UINavigationController(rootViewController: feed)
+        
+        window.rootViewController = root
         self.window = window
         window.makeKeyAndVisible()
+    }
+    
+    private func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "LegacyFeed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.delegate = delegate
+        feedController.title = title
+        return feedController
     }
 }
